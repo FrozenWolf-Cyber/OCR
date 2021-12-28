92b1b3d7dd4216333a08c1649ae398556c77dde4
Gokul Adethya <frozenwolf2003.com@gmail.com>
2021-12-28 07:20:30 +0530

    Add test flask app

---
+++ test_app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/predict',methods = ['POST', 'GET'])
+def login():
+   print(request.files,flush=True)
+   return "Done"
+
+if __name__ == '__main__':
+   app.run(debug=True,host='0.0.0.0', port=5000)
